572d9462347ab2ab4fd85dddf586f2728bc937df
Dany Ralantonisainana <lendormi1984@gmail.com>
2015-01-01 21:22:30 +0100

    [example] add support for ezpublish debug

--- modules/redis/example.php
+++ modules/redis/example.php
@@ -3,7 +3,7 @@
  * @author Dany RALANTONISAINANA <lendormi1984@gmail.com>
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or any later version)
  */
-
+header('Content-Type: text/xhtml; charset=utf-8');
 $http = eZHTTPTool::instance();
 
 $db = eZNoSqlDB::instance();
@@ -15,4 +15,8 @@ $query = array(
 $nodeListArray = $db->arrayQuery($query);
 echo $nodeListArray;
 
+echo "\r\n";
+eZDebug::printReport(false, false);
+echo "\r\n";
+eZDB::checkTransactionCounter();
 eZExecution::cleanExit();
